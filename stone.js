72d185d74b881582d7571b93e58843b96c5f7367
Suhani2826 <76738205+Suhani2826@users.noreply.github.com>
2021-05-05 14:55:32 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,24 @@
+class Stone {
+    constructor(x,y,width,height) {
+      var options = {
+        'density':12,
+        'friction':0.9,
+        'restitution':0.8
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      
+    }
+    
+    display(){
+      var angle = this.body.angle;
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
+  
+  
